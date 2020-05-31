d3a389131d5b180bd915f8bb518a86bac24f5ff1
LeThao98 <thaole4416@gmail.com>
2020-05-31 20:58:10 +0700

    lại lại

---
+++ server/build/precache-manifest.2cda6eb97995de9cd4dd2e4c74ae0e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da4eb13546cdddac833da4ad6791813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8935dc0d1f0cbd3d7cb8",
+    "url": "/static/css/2.bfebdfb8.chunk.css"
+  },
+  {
+    "revision": "b7cdc56dad74bf1e7d1b",
+    "url": "/static/css/main.1dedba75.chunk.css"
+  },
+  {
+    "revision": "8935dc0d1f0cbd3d7cb8",
+    "url": "/static/js/2.21a9084f.chunk.js"
+  },
+  {
+    "revision": "7cd9aaa00e3c8dd695bad5cb4f47ec3d",
+    "url": "/static/js/2.21a9084f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cdc56dad74bf1e7d1b",
+    "url": "/static/js/main.2618558d.chunk.js"
+  },
+  {
+    "revision": "1b498ecedcb012a3d521",
+    "url": "/static/js/runtime-main.79989eea.js"
+  }
+]);
